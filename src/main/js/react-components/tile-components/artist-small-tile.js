078a86218f80c6805359990ac6beac79710d158d
vsm22 <vssmirnov@gmail.com>
2018-08-05 17:35:57 -0500

    Style artist search result cards and authentication

---
+++ src/main/js/react-components/tile-components/artist-small-tile.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class ArtistSmallTile extends React.Component {
+
+
+    render() {
+
+        const artist = this.props.artist;
+
+        return() {
+
+            
+        }
+    }
+}
