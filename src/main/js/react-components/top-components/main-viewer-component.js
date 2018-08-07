9b1760802b6c876a01649d1b8661884afab87539
vsm22 <vssmirnov@gmail.com>
2018-08-07 16:30:29 -0500

    Move viewer routes into main router

--- src/main/js/react-components/top-components/main-viewer-component.js
+++ src/main/js/react-components/top-components/main-viewer-component.js
@@ -1,15 +1,5 @@
 import React from "react";
 import { Route, Redirect } from "react-router";
-import ArtistSearchResultViewerComponent from "../viewer-components/artist-search-result-viewer-component";
-import ArtistInfoViewerComponent from "../viewer-components/artist-info-viewer-component";
-import SpinnerComponent from "../viewer-components/spinner-component";
-import DevViewerComponent from "../viewer-components/dev-viewer-component";
-import BackgroundBarsGraphicComponent from "../../graphics/background-bars-graphic-component";
-import LoginViewerComponent from "../viewer-components/login-viewer-component";
-import LogoutViewerComponent from "../viewer-components/logout-viewer-component";
-import RegistrationViewerComponent from "../viewer-components/registration-viewer-component";
-import CreateArtistCollectionComponent from "../viewer-components/create-artist-collection-component";
-import GetArtistCollectionsComponent from "../viewer-components/get-artist-collections-component";
 
 class MainViewerComponent extends React.Component {
 
@@ -18,37 +8,8 @@ class MainViewerComponent extends React.Component {
         const props = this.props;
 
         return (
-
             <div className="wrap main-viewer-component-wrap">
-
-                <Route path="/login"
-                    render={(routeProps) => <LoginViewerComponent {...Object.assign({}, props, routeProps)} /> }
-                />
-
-                <Route path="/logout"
-                    render={(routeProps) => <LogoutViewerComponent {...Object.assign({}, props, routeProps)} /> }
-                />
-
-                <Route path="/register"
-                    render={(routeProps) => <RegistrationViewerComponent {...Object.assign({}, props, routeProps)} /> }
-                />
-
-                <Route path="/artistSearch"
-                    render={(routeProps) => <ArtistSearchResultViewerComponent {...Object.assign({}, props, routeProps)} /> }
-                />
-
-                <Route path="/artistInfo"
-                    render={(routeProps) => <ArtistInfoViewerComponent {...Object.assign({}, props, routeProps)} /> }
-                />
-
-                <Route path="/createArtistCollection"
-                    render={(routeProps) => <CreateArtistCollectionComponent {...Object.assign({}, props, routeProps)} /> }
-                />
-
-                <Route path="/getArtistCollections"
-                    render={(routeProps) => <GetArtistCollectionsComponent {...Object.assign({}, props, routeProps)} /> }
-                />
-
+                { this.props.viewerRoutes }
             </div>
         );
     }
